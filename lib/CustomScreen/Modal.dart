fe6ef9d536e8f6dedc30861f809bd37e591aa4e3
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-04-06 16:25:55 +0900

    fix modal

--- lib/CustomScreen/Modal.dart
+++ lib/CustomScreen/Modal.dart
@@ -8,22 +8,40 @@ class ShowModalScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Show Modal Screen"),
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (BuildContext context) {
-                  return ModalScreen();
-                },
-                fullscreenDialog: true
-              )
-            );
-          },
-          child: Text("Show Modal"),
-        ),
-      ),
+      body: Stack(
+        children: <Widget>[
+          Center(
+            child: RaisedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) {
+                          return ModalScreen();
+                        },
+                        fullscreenDialog: true
+                    )
+                );
+              },
+              child: Text("Show Modal"),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 64, left: 64),
+              child: Container(
+                height: 64,
+                width: 64,
+                child: Hero(
+                  tag: 'tag',
+                  child: Image.network("http://t1.gstatic.com/images?q=tbn:ANd9GcS_9ql4aU2vamO8FtoNPO8NDl2B-15FmP3wCFEc2-I4fWyTz9aJkfdD0dGh_HdVTT16iERKgelvABWEOm_RBJw"),
+                ),
+              ),
+            )
+          )
+        ],
+      )
     );
   }
 }
@@ -37,7 +55,27 @@ class ModalScreen extends StatelessWidget {
         title: Text("Modal Screen"),
       ),
       body: Center(
-        child: Text("This is Modal Screen"),
+        child: Stack(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 64, left: 64, right: 64),
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Hero(
+                  tag: 'tag',
+                  child: Image.network("http://t1.gstatic.com/images?q=tbn:ANd9GcS_9ql4aU2vamO8FtoNPO8NDl2B-15FmP3wCFEc2-I4fWyTz9aJkfdD0dGh_HdVTT16iERKgelvABWEOm_RBJw"),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 64),
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Text("This is Modal Screen"),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
