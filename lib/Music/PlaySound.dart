9e3c687d2fc6fca1989cbd7bfbb427858cd99573
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-04-07 20:27:46 +0900

    add PlaySoundScreen

---
+++ lib/Music/PlaySound.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+class PlaySoundScreen extends StatefulWidget {
+  @override
+  _PlaySoundScreenState createState() => _PlaySoundScreenState();
+}
+
+class _PlaySoundScreenState extends State<PlaySoundScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
